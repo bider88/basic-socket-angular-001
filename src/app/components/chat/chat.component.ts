293cc360d4c457c0415e9b42029a6f4d5025b8ef
Irving Didier <bider88@gmail.com>
2019-05-02 01:49:19 -0500

    Adding style to component chat

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { ChatService } from 'src/app/services/chat.service';
 import { Subscription } from 'rxjs';
+import { PayloadInterface } from 'src/app/interfaces/payload.interface';
 
 @Component({
   selector: 'app-chat',
@@ -11,6 +12,9 @@ export class ChatComponent implements OnInit, OnDestroy {
 
   text = '';
   subscriptor: Subscription;
+  messages: PayloadInterface[] = [];
+  scrollTop: number;
+  @ViewChild('messageRef') messageRef: ElementRef;
 
   constructor(
     private chatService: ChatService
@@ -25,13 +29,19 @@ export class ChatComponent implements OnInit, OnDestroy {
   }
 
   send() {
-    this.chatService.sendMessage(this.text);
+    if (this.text.trim().length) {
+      this.chatService.sendMessage(this.text);
+    }
     this.text = '';
   }
 
   receive() {
     this.subscriptor = this.chatService.receiveMessage().subscribe(
-      msg => console.log(`Receiving message...`, msg)
+      (msg: PayloadInterface) =>  {
+        this.messages.push(msg);
+
+        setTimeout(() => this.messageRef.nativeElement.scrollTop = this.messageRef.nativeElement.scrollHeight, 0);
+      }
     );
   }
 
