b3c464b02e883e948f38b195879ea3213b30a923
Irving Didier <bider88@gmail.com>
2019-06-25 23:27:55 -0500

    Adding styles to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RoutingModule } from './routing.module';
 
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
@@ -26,7 +27,8 @@ const config: SocketIoConfig = { url: environment.wsUrl, options: {} };
   imports: [
     BrowserModule,
     SocketIoModule.forRoot(config),
-    FormsModule
+    FormsModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
